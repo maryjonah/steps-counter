7e9ecbd7a0acc153fa7357dcf6e7e1ddef0621e9
Mary Jonah <jonahmary17@gmail.com>
2023-09-29 20:56:17 +0000

    Upload files for steps counter

---
+++ index.js
@@ -0,0 +1,20 @@
+// 1. Grab the save-el paragraph and store it in a variable called saveEl
+let saveEl = document.getElementById("save-el");
+let countEl = document.getElementById("count-el");
+let count = 0;
+
+function increment() {
+    count += 1;
+    countEl.textContent = count;
+    // count = 0;
+}
+
+function save() {
+    // 2. Create a variable that contains both the count and the dash separator, i.e. "12 - "
+    // 3. Render the variable in the saveEl using innerText
+    // NB: Make sure to not delete the existing content of the paragraph
+    let countStr = count + " - ";
+    saveEl.textContent += countStr;
+    count = 0;
+    countEl.textContent = count;
+}
